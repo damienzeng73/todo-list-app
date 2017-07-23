5a44e4425badb33a0031f061c44f8b603ad00478
DamienZeng <damnee562@gmail.com>
2017-07-23 22:36:05 +0800

    Add todo form.

--- src/js/containers/ListContainer.jsx
+++ src/js/containers/ListContainer.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
+import AddItemForm from '../components/AddItemForm'
 import List from '../components/List'
 
 class ListContainer extends React.Component {
     render() {
         return (
             <div id='todo-list-container'>
+                <AddItemForm />
                 <List />
             </div>
         )
