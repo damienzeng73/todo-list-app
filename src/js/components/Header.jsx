017be4c762045b9ebd8dc4143e1b835fd6a8cd04
DamienZeng <damnee562@gmail.com>
2017-07-23 22:39:25 +0800

    Using semantic-ui Container instead of custom css.

--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Header } from 'semantic-ui-react'
+import { Container, Header } from 'semantic-ui-react'
 
 const _Header = () => {
     return (
-        <header id='header'>
+        <Container id='header' fluid>
             <Header as='h1'>TODO LIST</Header>
-        </header>
+        </Container>
     )
 }
 
