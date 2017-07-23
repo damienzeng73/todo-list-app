c05725ad075e8e333b6f0d248665e3851744b0b2
DamienZeng <damnee562@gmail.com>
2017-07-23 21:57:36 +0800

    Add Header.

---
+++ src/js/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Header } from 'semantic-ui-react'
+
+const _Header = () => {
+    return (
+        <header id='header'>
+            <Header as='h1'>TODO LIST</Header>
+        </header>
+    )
+}
+
+
+export default _Header
--- src/js/containers/App.jsx
+++ src/js/containers/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import Header from '../components/Header'
+
 class App extends React.Component {
     render() {
         return (
-            <h1>What up biatch!</h1>
+            <Header />
         )
     }
 }
